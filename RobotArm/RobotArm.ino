21b25b5642ceec25e2a0dd8d98ad2c2af951fb1f
DavidRhysGemmell <58792916+DavidRhysGemmell@users.noreply.github.com>
2022-04-01 16:35:00 +0100

    Update RobotArm.ino

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -30,29 +30,30 @@ int Servo5Angle=0;
 int Servo6Angle=0;
 
 
-float d_1 = 77.6; //Height to first joint
-#define l_1 150 // Lenght of first arm
-#define l_2 150 // Length of second arm
-#define l_3 50 // Length of third arm
+double d_1 = 77.6; //Height to first joint
+int l_1=150; // Lenght of first arm
+int l_2=150;  // Length of second arm
+int l_3=50; // Length of third arm
 
 int x = 150; //Initalise End coordinates
-int y = 50;
-int z = 50;
+int y = 100;
+int z = 87;
+
 float phi = PI; // End posture of arm, in radians. Could be editable.
 
 float xp = 0;
 float zp = 0;
-double ctheta_1 = 0;
-double stheta_1 = 0;
-double theta_1 = 0;
-double x3 = 0;
-double y3 = 0;
-double z3 = 0;
-double x3p = 0;
-double z3p = 0;
+float ctheta_1 = 0;
+float stheta_1 = 0;
+float theta_1 = 90;
+float x3 = 0;
+float y3 = 0;
+float z3 = 0;
+float x3p = 0;
+float z3p = 0;
 double ctheta_3 = 0;
 double stheta_3 = 0;
-double theta_3 = 0;
+double theta_3 = 45;
 double sbeta = 0;
 double cbeta = 0;
 double beta = 0;
@@ -66,51 +67,12 @@ double theta_2deg = 0;
 double theta_3deg = 0;
 double theta_4deg = 0;
 int Channel1Position = 90;
-int Channel2Position = 90;
-int Channel3Position = 90;
-int Channel3PrevPosition = 90;
-double MaxReach = sqrt(pow(l_1,2)+pow(l_2,2)+pow(50*sin(phi),2));
+int Channel2Position = 45;
+int Channel3Position = 45;
+int Channel3PrevPosition = 45;
+double MaxReach = sqrt(pow(l_1,2)+pow(l_2,2));
 double Current = 0;
-void CurrentReach(void){
-  Current = 115; // Work this out, like sqrt x^2+ y^2+z-77.6^2
-}
-void CalculateAngles(void){
-  xp=sqrt(pow(x,2)+pow(y,2));
-  zp=z-d_1;
-  ctheta_1=x/xp;
-  stheta_1=y/xp;
-  theta_1 = atan2(stheta_1,ctheta_1);
-  x3=x-l_3*sin(phi)*cos(theta_1);
-  y3=y-l_3*sin(phi)*sin(theta_1);
-  z3=z-l_3*cos(phi);
-  x3p=sqrt(pow(x3,2)+pow(y3,2));
-  z3p=z3-d_1;
-  ctheta_3= ((pow(x3p,2))+(pow(z3p,2))-(pow(l_1,2))-(pow(l_2,2)))/(2*l_1*l_2);
-  stheta_3= -sqrt(1-pow(ctheta_3,2));
-  theta_3 = atan2(stheta_3,ctheta_3);
-  sbeta= z3p/(sqrt((pow(x3p,2))+(pow(z3p,2))));
-  cbeta = x3p/(sqrt((pow(x3p,2))+(pow(z3p,2))));
-  beta = atan2(sbeta,cbeta);
-  cpsi= ((pow(x3p,2))+(pow(z3p,2))+(pow(l_1,2))-(pow(l_2,2)))/(2*l_1*(sqrt((pow(x3p,2))+(pow(z3p,2)))));
-  spsi= (l_2*stheta_3)/(sqrt((pow(x3p,2))+(pow(z3p,2))));
-  psi= atan2(spsi,cpsi);
-  theta_2 = beta-psi;
-  theta_4= phi-theta_2-theta_3;
-  //convert radians to degrees
-  theta_1deg= theta_1*180/PI;
-  theta_2deg= theta_2*180/PI;
-  theta_3deg= theta_3*180/PI;
-  theta_4deg= theta_4*180/PI;
-    Serial.print(" Theta 1: ");
-  Serial.print(theta_1deg);
-    Serial.print(" Theta 2: ");
-  Serial.print(theta_2deg);
-    Serial.print(" Theta 3: ");
-  Serial.print(theta_3deg);
-    Serial.print(" Theta 4: ");
-  Serial.print(theta_4deg);
-  Serial.println();
-}
+
 void setup() {
   Serial.begin(115200);
   Servo1.attach(Channel1); 
@@ -125,7 +87,52 @@ void setup() {
 //  attachPCINT(digitalPinToPCINT(7), MoveServo6, CHANGE);
 }
 
+void CurrentReach(void){
+  Current = sqrt(pow(x3p,2)+pow(z3p,2)); // Work this out, like sqrt x^2+ y^2+z-77.6^2
+}
+void CalculateAngles(void){
 
+  //Calculate Theta_1
+  xp=sqrt(pow(x,2)+pow(y,2));
+  zp=z-d_1;
+  ctheta_1=x/xp;
+  stheta_1=y/xp;
+  theta_1 = atan2(stheta_1,ctheta_1);
+  //Calculate the joint before the end one
+x3=x-l_3*sin(phi)*cos(theta_1);
+y3=y-l_3*sin(phi)*sin(theta_1);
+z3=z-l_3*cos(phi);
+
+  x3p=sqrt(pow(x3,2)+pow(y3,2));
+  z3p=z3-d_1;
+  
+  ctheta_3= ((pow(x3p,2))+(pow(z3p,2))-(pow(l_1,2))-(pow(l_2,2)))/(45000);
+  stheta_3= -sqrt(1-pow(ctheta_3,2));
+  theta_3 = atan2(stheta_3,ctheta_3);
+  sbeta= z3p/(sqrt((pow(x3p,2))+(pow(z3p,2))));
+  cbeta = x3p/(sqrt((pow(x3p,2))+(pow(z3p,2))));
+  beta = atan2(sbeta,cbeta);
+  cpsi= ((pow(x3p,2))+(pow(z3p,2))+(pow(l_1,2))-(pow(l_2,2)))/(2*l_1*(sqrt((pow(x3p,2))+(pow(z3p,2)))));
+  spsi= (l_2*stheta_3)/(sqrt((pow(x3p,2))+(pow(z3p,2))));
+  psi= atan2(spsi,cpsi);
+  theta_2 = beta-psi;
+  theta_4= phi-theta_2-theta_3;
+
+  //convert radians to degrees
+  theta_1deg= theta_1*180/PI;
+  theta_2deg= theta_2*180/PI;
+  theta_3deg= -theta_3*180/PI;
+  theta_4deg= 360-(theta_4*180/PI);
+    Serial.print(" Theta 1: ");
+  Serial.print(theta_1deg);
+    Serial.print(" Theta 2: ");
+  Serial.print(theta_2deg);
+    Serial.print(" Theta 3: ");
+  Serial.print(theta_3deg);
+    Serial.print(" Theta 4: ");
+  Serial.print(theta_4deg);
+  Serial.println();
+}
 void MoveServo1(void){
   Serial.print("Ch1: ");
   Serial.print(ch1.getAngle());
@@ -171,11 +178,10 @@ void CheckXAxis(void){
   CurrentReach();
   Channel1Position = ch1.getAngle();
   
-  if (Channel1Position>=105){
-    if (x<MaxReach){ //Hopefully doesn't freak out if x is decimal just below reach
+  if (Channel1Position>=105 && theta_3deg>0){
     x=x+1;
-    }
-  } else if (Channel1Position<=75){
+    
+  } else if (Channel1Position<=75 && theta_3deg>0){
     x=x-1;
   }
         Serial.print(" X is: ");
@@ -185,11 +191,11 @@ void CheckYAxis(void){
   CurrentReach();
   Channel2Position = ch2.getAngle();
   
-  if (Channel2Position>=105){
-    if (Current<MaxReach){ //Hopefully doesn't freak out if x is decimal just below reach
+  if (Channel2Position>=105 && theta_3deg>0){
+     //Hopefully doesn't freak out if x is decimal just below reach
     y=y+1;
-    }
-  } else if (Channel2Position<=75){
+    
+  } else if (Channel2Position<=75 && y>0){ //y has to be greater than 0 to keep with rotation of servos
     y=y-1;
   }
         Serial.print(" Y is ");
@@ -202,25 +208,32 @@ void CheckZAxis(void){
   
   if (Channel3Position<=Channel3PrevPosition){
      z=Channel3Position;
-  } else if (Channel3Position>Channel3PrevPosition && Current<MaxReach){
+  } else if (Channel3Position>Channel3PrevPosition && theta_3deg>0){
     z=Channel3Position;
   }
   Channel3PrevPosition=Channel3Position;
         Serial.print(" Z is ");
   Serial.print(z);
 }
+
+void MakeSureServosAreNotOutOfAngle(void){
+  if (y<0){
+    y=0; //Limits rotation of base to positive y. ie theta_1>0
+  }
+}
 void loop() {
   CheckXAxis();
   CheckYAxis();
   CheckZAxis();
+  MakeSureServosAreNotOutOfAngle();
   CalculateAngles();
-  MoveServo1();
-  
+  MoveServo1(); 
   MoveServo2();
   MoveServo3();
   MoveServo4();
   MoveServo5();
   MoveServo6();
+
   Serial.println();
-  delay(50);
+  delay(200);
 }
